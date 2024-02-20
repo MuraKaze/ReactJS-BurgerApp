b27f2e6dc65991dfc036b2e536131bf6bf6c0b21
MuraKaze <ahmalik99@gmail.com>
2024-02-20 12:32:14 +0500

    Replaced images with CSS templates

--- src/components/burger.js
+++ src/components/burger.js
@@ -6,14 +6,30 @@ export default class Burger extends Component {
     lettuce: 0,
     tomato: 0,
     cheese: 0,
-    meat: 0
+    meat: 0,
+    price: 3.00,
   }
 
-  changeIngridient = (action, ingridient) => {
-    this.setState(prevState => ({
-      [ingridient]: Math.max(action === 'add' ? prevState[ingridient] + 1 : prevState[ingridient] - 1, 0)
-    }));
+  changeIngridient = (action, ingredient) => {
+    const ingredientCost = {
+      lettuce: 0.50,
+      tomato: 0,
+      cheese: 0.40,
+      meat: 1.30
+    };
+
+    const costChange = action === 'add' ? ingredientCost[ingredient] : -ingredientCost[ingredient];
+    const newPrice = Math.max(this.state.price + costChange, 0);
+
+    if (action === 'add' || this.state[ingredient] > 0) {
+      this.setState(prevState => ({
+        [ingredient]: Math.max(action === 'add' ? prevState[ingredient] + 1 : prevState[ingredient] - 1, 0),
+        price: newPrice
+      }));
+    }
   }
+
+
 
   renderBurgerIngredients = () => {
     const { lettuce, tomato, cheese, meat } = this.state;
@@ -39,26 +55,37 @@ export default class Burger extends Component {
           {this.renderBurgerIngredients()}
           <div className="bottom side"></div>
         </div>
-        <div className="ingridientsBlock">
-          <p>Lettuce</p>
-          <div className="ingrBtns">
-            <button onClick={() => this.changeIngridient('add', 'lettuce')} className="ingrBtn">Add</button>
-            <button onClick={() => this.changeIngridient('remove', 'lettuce')} className="ingrBtn">Remove</button>
+        <div className="ingridientsBlocks">
+        <div className="currentPrice">
+          Current Price: <strong>${this.state.price.toFixed(2)}</strong>
+        </div>
+          <div className="igridientsBlock">
+            <p>Lettuce</p>
+            <div className="ingrBtns">
+              <button onClick={() => this.changeIngridient('add', 'lettuce')} className="ingrBtn">Add</button>
+              <button onClick={() => this.changeIngridient('remove', 'lettuce')} className="ingrBtn">Remove</button>
+            </div>
           </div>
-          <p>Tomato</p>
-          <div className="ingrBtns">
-            <button onClick={() => this.changeIngridient('add', 'tomato')} className="ingrBtn">Add</button>
-            <button onClick={() => this.changeIngridient('remove', 'tomato')} className="ingrBtn">Remove</button>
+          <div className="igridientsBlock">
+            <p>Tomato</p>
+            <div className="ingrBtns">
+              <button onClick={() => this.changeIngridient('add', 'tomato')} className="ingrBtn">Add</button>
+              <button onClick={() => this.changeIngridient('remove', 'tomato')} className="ingrBtn">Remove</button>
+            </div>
           </div>
-          <p>Cheese</p>
-          <div className="ingrBtns">
-            <button onClick={() => this.changeIngridient('add', 'cheese')} className="ingrBtn">Add</button>
-            <button onClick={() => this.changeIngridient('remove', 'cheese')} className="ingrBtn">Remove</button>
+          <div className="igridientsBlock">
+            <p>Cheese</p>
+            <div className="ingrBtns">
+              <button onClick={() => this.changeIngridient('add', 'cheese')} className="ingrBtn">Add</button>
+              <button onClick={() => this.changeIngridient('remove', 'cheese')} className="ingrBtn">Remove</button>
+            </div>
           </div>
-          <p>Meat</p>
-          <div className="ingrBtns">
-            <button onClick={() => this.changeIngridient('add', 'meat')} className="ingrBtn">Add</button>
-            <button onClick={() => this.changeIngridient('remove', 'meat')} className="ingrBtn">Remove</button>
+          <div className="igridientsBlock">
+            <p>Meat</p>
+            <div className="ingrBtns">
+              <button onClick={() => this.changeIngridient('add', 'meat')} className="ingrBtn">Add</button>
+              <button onClick={() => this.changeIngridient('remove', 'meat')} className="ingrBtn">Remove</button>
+            </div>
           </div>
         </div>
       </>
